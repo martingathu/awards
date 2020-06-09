d43153a639d30419a408b9be147dbe1c8404ae03
martingathu <martin5gathu@gmail.com>
2020-06-09 16:27:00 +0300

    add authentication to the post service

--- awardsapp/permissions.py
+++ awardsapp/permissions.py
@@ -1,5 +1,6 @@
 from rest_framework.permissions import SAFE_METHODS, BasePermission
 
+# add class here
 class IsAdminOrReadOnly(BasePermission):
     def has_permission(self, request, view):
         if request.method in SAFE_METHODS:
